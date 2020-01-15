8f73c816da3b9ed45a2393b5d40cf0a16c161501
Brian Bachow <bmbachow@gmail.com>
2020-01-15 13:22:58 -0800

    122pm

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function getDogImage() {
-  fetch('https://dog.ceo/api/breeds/image/random')
+  fetch(`https://dog.ceo/api/breeds/image/random/${dogimgs.value}`)
     .then(response => response.json())
     .then(responseJson => 
       displayResults(responseJson))
@@ -13,19 +13,33 @@ function displayResults(responseJson) {
   //replace the existing image with the new one
   $('.results-img').replaceWith(
     `<img src="${responseJson.message}" class="results-img">`
-  )
+  );
   //display the results section
   $('.results').removeClass('hidden');
 }
 
-function watchForm() {
-  $('form').submit(event => {
-    event.preventDefault();
-    getDogImage();
-  });
+function watchForm()
+{ $('form').submit(event => { event.preventDefault(); 
+  console.log(document.getElementById('dogimgs').value); 
+  getDogImage();
+});
 }
+
 
 $(function() {
   console.log('App loaded! Waiting for submit!');
   watchForm();
 });
+
+
+
+
+/* 
+
+have form where user selects a number between 1 and 50 that defaults to 3
+
+user selects number and clicks submit
+
+app shows that number of random dog images
+
+*/
